a075705e24e22354e6bd9d82f7ea77eff3d4a5d8
Nathan <nathanellis15782@protonmail.com>
2021-11-22 18:47:59 +0000

    Edited README and added to the game

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # God Speed
 A racing game that is a work in progress
+
+## Credit
+The following work was used to make this game: \
+Title: Road for 2D Games \
+Author: the_black_box \
+URL: [https://opengameart.org/content/road-for-2d-games](https://opengameart.org/content/road-for-2d-games) \
+License: [https://creativecommons.org/licenses/by/4.0/](https://creativecommons.org/licenses/by/4.0/)
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import pygame
 from pygame import *
+import random
+import os
 pygame.init()
 
 running = True
+
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((500, 505))
+pygame.display.set_caption("God Speed")
+#gameicon = pygame.image.load(os.path.join("assets", "icon.ico"))
+#pygame.display.set_icon(gameicon)
+
+blue = (25, 99, 145)
+
+road = pygame.image.load(os.path.join("assets", "road.svg"))
+
+def redraw():
+    clock.tick(30)
+    pygame.display.update()
+    screen.fill(blue)
+    screen.blit(road, (200, 0))
 
 while running:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
+    
+    redraw()
