996830aa90c7c2080caa816cd25cf63d88268a48
Nathan <nathanellis15782@protonmail.com>
2021-11-22 20:00:58 +0000

    Added background and player icon as well as movement and added credit for the sprites

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ The following work was used to make this game: \
 Title: Road for 2D Games \
 Author: the_black_box \
 URL: [https://opengameart.org/content/road-for-2d-games](https://opengameart.org/content/road-for-2d-games) \
-License: [https://creativecommons.org/licenses/by/4.0/](https://creativecommons.org/licenses/by/4.0/)
+License: [https://creativecommons.org/licenses/by/4.0/](https://creativecommons.org/licenses/by/4.0/) \
+Title: Race Car \
+Author: rrcaseyr \
+URL: [https://opengameart.org/content/race-car](https://opengameart.org/content/race-car) \
+License: [http://creativecommons.org/licenses/by/3.0/](http://creativecommons.org/licenses/by/3.0/)
--- main.py
+++ main.py
@@ -8,24 +8,75 @@ running = True
 
 clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((500, 505))
+screen = pygame.display.set_mode((500, 478))
 pygame.display.set_caption("God Speed")
 #gameicon = pygame.image.load(os.path.join("assets", "icon.ico"))
 #pygame.display.set_icon(gameicon)
 
 blue = (25, 99, 145)
 
-road = pygame.image.load(os.path.join("assets", "road.svg"))
+#The Car
+carimport = [
+    pygame.image.load(os.path.join("assets", "car1.png")),
+    pygame.image.load(os.path.join("assets", "car2.png"))
+    ]
+carstage = 0
+car = []
+for c in carimport:
+    car.append(pygame.transform.scale(c, (64, 64)))
+#The car change timer
+pygame.time.set_timer(USEREVENT+1, 150)
+
+#The background
+road = pygame.image.load(os.path.join("assets", "road.png")) #270 x 478
+bgy1 = 0
+bgy2 = road.get_height() * -1
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 20
+        self.height = 30
+    def draw(self):
+        screen.blit(car[carstage], (self.x, self.y))
+
+char = Player(153, 400)
 
 def redraw():
+    global bgy1, bgy2
     clock.tick(30)
     pygame.display.update()
     screen.fill(blue)
-    screen.blit(road, (200, 0))
+    screen.blit(road, (105, bgy1))
+    screen.blit(road, (105, bgy2))
+    if bgy1 >= 478:
+        bgy1 = road.get_height() * -1
+    if bgy2 >= 478:
+        bgy2 = road.get_height() * -1
+    char.draw()
+
+def movement():
+    keypressed = pygame.key.get_pressed()
+
+    if keypressed[K_RIGHT] and char.x == 153:
+        char.x += 109
+    if keypressed[K_LEFT] and char.x == 262:
+        char.x -= 109
 
 while running:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
+        if e.type == USEREVENT+1:
+            carstage += 1
     
+    bgy1 += 1
+    bgy2 += 1
+
+    if carstage >= 2:
+        carstage = 0
+
+    movement()
+
     redraw()
