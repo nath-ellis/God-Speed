a15c90501e316b44139c89f203baaa12d564024a
Nathan <nathanellis15782@protonmail.com>
2021-12-01 16:38:20 +0000

    Added a game icon

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((500, 478))
 pygame.display.set_caption("God Speed")
-#gameicon = pygame.image.load(os.path.join("assets", "icon.ico"))
-#pygame.display.set_icon(gameicon)
+gameicon = pygame.image.load(os.path.join("assets", "car1.png"))
+pygame.display.set_icon(gameicon)
 
 blue = (25, 99, 145)
 white = (255, 255, 255)
