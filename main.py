c90511bebaf35c0e307c214cfdf3bb611fa9fb04
nathan <86123602+noxJ@users.noreply.github.com>
2022-01-04 17:05:09 +0000

    Renamed variables and cleaned up the code

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((500, 478))
 pygame.display.set_caption("God Speed")
-gameicon = pygame.image.load(os.path.join("assets", "car1.png"))
-pygame.display.set_icon(gameicon)
+game_icon = pygame.image.load(os.path.join("assets", "car1.png"))
+pygame.display.set_icon(game_icon)
 
 blue = (25, 99, 145)
 white = (255, 255, 255)
@@ -24,37 +24,37 @@ black = (0, 0, 0)
 red = (250, 9, 18)
 
 # The Car
-carimport = [
+car_import = [
     pygame.image.load(os.path.join("assets", "car1.png")),
     pygame.image.load(os.path.join("assets", "car2.png"))
 ]
-carstage = 0
+car_stage = 0
 car = []
-for c in carimport:
+for c in car_import:
     car.append(pygame.transform.scale(c, (64, 64)))
 # The car change timer
 pygame.time.set_timer(USEREVENT + 1, 150)
 
 # The background
-road = pygame.transform.scale(pygame.image.load(os.path.join("assets", "road.png")), ((270, 600)))  # 270 x 478
+road = pygame.transform.scale(pygame.image.load(os.path.join("assets", "road.png")), (270, 600))  # 270 x 478
 bgy1 = 0
 bgy2 = road.get_height() * -1
 
 # Obstacles
-enemycar1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "enemycar1.png")), ((46, 96)))
-enemycar2 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "enemycar2.png")), ((46, 96)))
-enemycar3 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "enemycar3.png")), ((46, 96)))
-enemycar4 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "enemycar4.png")), ((46, 96)))
-truck1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "truck1.png")), ((46, 96)))
+enemy_car_1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "enemycar1.png")), (46, 96))
+enemy_car_2 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "enemycar2.png")), (46, 96))
+enemy_car_3 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "enemycar3.png")), (46, 96))
+enemy_car_4 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "enemycar4.png")), (46, 96))
+truck1 = pygame.transform.scale(pygame.image.load(os.path.join("assets", "truck1.png")), (46, 96))
 obstacles = []
 pygame.time.set_timer(USEREVENT + 2, 1000)
 pygame.time.set_timer(USEREVENT + 4, 3000, 5000)
 pygame.time.set_timer(USEREVENT + 5, 2000, 3000)
 pygame.time.set_timer(USEREVENT + 6, 1500, 2000)
 pygame.time.set_timer(USEREVENT + 7, 1000, 1500)
-enemyspeed = 5
+enemy_speed = 5
 
-explosionsfx = pygame.mixer.Sound(os.path.join("assets", "explosion.flac"))
+explosion_sfx = pygame.mixer.Sound(os.path.join("assets", "explosion.flac"))
 
 # Lives
 life = pygame.image.load(os.path.join("assets", "lives.png"))
@@ -62,12 +62,12 @@ life = pygame.image.load(os.path.join("assets", "lives.png"))
 ticks = 0
 pygame.time.set_timer(USEREVENT + 3, 1000)
 
-credittimer = 0
+credit_timer = 0
 
 # Text
 text = pygame.font.Font(os.path.join("assets", "kenpixel_blocks.ttf"), 50)
-smalltext = pygame.font.Font(os.path.join("assets", "kenpixel_blocks.ttf"), 20)
-mediumtext = pygame.font.Font(os.path.join("assets", "kenpixel_blocks.ttf"), 25)
+small_text = pygame.font.Font(os.path.join("assets", "kenpixel_blocks.ttf"), 20)
+medium_text = pygame.font.Font(os.path.join("assets", "kenpixel_blocks.ttf"), 25)
 
 # Music
 pygame.mixer.music.load(os.path.join("assets", "MenuSong.mp3"))
@@ -79,10 +79,10 @@ md.set_markdown(os.path.join("assets", "credit.md"))
 
 # Score
 score = 0
-hscore1 = open(os.path.join("assets", "highscore.txt"), "r")
-hscore1contents = hscore1.readlines()
-highscore = int(hscore1contents[0])
-hscore1.close()
+high_score_1 = open(os.path.join("assets", "highscore.txt"), "r")
+high_score_1_contents = high_score_1.readlines()
+high_score = int(high_score_1_contents[0])
+high_score_1.close()
 
 
 class Player:
@@ -95,32 +95,32 @@ class Player:
         self.lives = 3
 
     def draw(self):
-        screen.blit(car[carstage], (self.x, self.y))
+        screen.blit(car[car_stage], (self.x, self.y))
 
 
 char = Player(153, 400)
 
 
 class Car:
-    def __init__(self, x, y, type):
+    def __init__(self, x, y, style):
         self.x = x
         self.y = y
         self.width = 46
         self.height = 96
-        self.type = type
+        self.type = style
 
     def draw(self):
         if self.type == "blue1":
-            screen.blit(enemycar1, (self.x, self.y))
+            screen.blit(enemy_car_1, (self.x, self.y))
         elif self.type == "green":
-            screen.blit(enemycar2, (self.x, self.y))
+            screen.blit(enemy_car_2, (self.x, self.y))
         elif self.type == "blue2":
-            screen.blit(enemycar3, (self.x, self.y))
+            screen.blit(enemy_car_3, (self.x, self.y))
         elif self.type == "pink":
-            screen.blit(enemycar4, (self.x, self.y))
+            screen.blit(enemy_car_4, (self.x, self.y))
 
     def move(self):
-        self.y += enemyspeed
+        self.y += enemy_speed
 
 
 def redraw():
@@ -136,7 +136,7 @@ def redraw():
         if bgy2 >= 468:
             bgy2 = (road.get_height() - char.speed * 3) * -1
         char.draw()
-        screen.blit(mediumtext.render(str(score), True, white), (5, 0))
+        screen.blit(medium_text.render(str(score), True, white), (5, 0))
         # Lives
         if char.lives == 0:
             gameOver = True
@@ -169,11 +169,11 @@ def redraw():
 
 
 def movement():
-    keypressed = pygame.key.get_pressed()
+    key_pressed = pygame.key.get_pressed()
 
-    if keypressed[K_RIGHT] and char.x == 153 or keypressed[K_d] and char.x == 153:
+    if key_pressed[K_RIGHT] and char.x == 153 or key_pressed[K_d] and char.x == 153:
         char.x += 109
-    if keypressed[K_LEFT] and char.x == 262 or keypressed[K_a] and char.x == 262:
+    if key_pressed[K_LEFT] and char.x == 262 or key_pressed[K_a] and char.x == 262:
         char.x -= 109
 
 
@@ -215,7 +215,7 @@ def obstacle():
 
 
 def explosion():
-    explosionimg = [
+    explosion_img = [
         pygame.image.load(os.path.join("assets", "explosion1.png")),
         pygame.image.load(os.path.join("assets", "explosion2.png")),
         pygame.image.load(os.path.join("assets", "explosion3.png")),
@@ -229,20 +229,20 @@ def explosion():
         pygame.image.load(os.path.join("assets", "explosion11.png")),
         pygame.image.load(os.path.join("assets", "explosion12.png"))
     ]
-    pygame.mixer.Sound.play(explosionsfx)
-    for e in explosionimg:
-        screen.blit(e, (char.x - 20, char.y - 18))
+    pygame.mixer.Sound.play(explosion_sfx)
+    for ex in explosion_img:
+        screen.blit(ex, (char.x - 20, char.y - 18))
         pygame.display.update()
         clock.tick(24)
 
 
 def lose():
-    global gameOver, obstacles, ticks, enemyspeed, bgy1, bgy2, score, highscore
+    global gameOver, obstacles, ticks, enemy_speed, bgy1, bgy2, score, high_score
 
-    screen.blit(text.render("Game", True, (red)), (165, 50))
-    screen.blit(text.render("Over", True, (red)), (170, 100))
-    screen.blit(mediumtext.render(str(score), True, (white)), (5, 0))
-    screen.blit(smalltext.render("Click to play", True, (white)), (152, 350))
+    screen.blit(text.render("Game", True, red), (165, 50))
+    screen.blit(text.render("Over", True, red), (170, 100))
+    screen.blit(medium_text.render(str(score), True, white), (5, 0))
+    screen.blit(small_text.render("Click to play", True, white), (152, 350))
 
     bgy1 = 0
     bgy2 = road.get_height() * -1
@@ -253,14 +253,14 @@ def lose():
 
     ticks = 0
 
-    if score > highscore:
-        hscore2 = open(os.path.join("assets", "highscore.txt"), "w")
-        hscore2.write(str(score))
-        hscore2.close()
-        highscore = score
+    if score > high_score:
+        high_score_2 = open(os.path.join("assets", "highscore.txt"), "w")
+        high_score_2.write(str(score))
+        high_score_2.close()
+        high_score = score
 
     char.speed = 1
-    enemyspeed = 5
+    enemy_speed = 5
 
     mouseclick = pygame.mouse.get_pressed()
 
@@ -270,41 +270,41 @@ def lose():
 
 
 def menu():
-    global menuOpen, enemyspeed, credit, credittimer
+    global menuOpen, enemy_speed, credit, credit_timer
     mouseclick = pygame.mouse.get_pressed()
-    mousex, mousey = pygame.mouse.get_pos()
+    mouse_x, mouse_y = pygame.mouse.get_pos()
 
     if not credit:
-        screen.blit(text.render("God", True, (white)), (185, 50))
-        screen.blit(text.render("Speed", True, (white)), (150, 100))
-        screen.blit(smalltext.render("Click to play", True, (white)), (152, 350))
-        screen.blit(smalltext.render("Credit", True, (white)), (5, 448))
-        screen.blit(mediumtext.render(str(highscore), True, (white)), (5, 0))
+        screen.blit(text.render("God", True, white), (185, 50))
+        screen.blit(text.render("Speed", True, white), (150, 100))
+        screen.blit(small_text.render("Click to play", True, white), (152, 350))
+        screen.blit(small_text.render("Credit", True, white), (5, 448))
+        screen.blit(medium_text.render(str(high_score), True, white), (5, 0))
     else:
         screen.fill(blue)
-        screen.blit(smalltext.render("The following work was used", True, (white)), (55, 10))
-        screen.blit(smalltext.render("Go Back", True, (white)), (5, 448))
+        screen.blit(small_text.render("The following work was used", True, white), (55, 10))
+        screen.blit(small_text.render("Go Back", True, white), (5, 448))
         md.set_area(screen, 0, 40, width=500, height=415)
         md.set_color_background(25, 99, 145)
         md.set_color_font(255, 255, 255)
-        md.display(pygame.event.get(), mousex, mousey, mouseclick)
+        md.display(pygame.event.get(), mouse_x, mouse_y, mouseclick)
 
     char.speed = 1
-    enemyspeed = 5
-    credittimer -= 1
+    enemy_speed = 5
+    credit_timer -= 1
 
-    if credittimer <= 0:
-        credittimer = 0
+    if credit_timer <= 0:
+        credit_timer = 0
 
     if mouseclick[0]:
         pos = pygame.mouse.get_pos()
-        if pos[0] < 90 and pos[1] > 450 and not credit and credittimer <= 0:
-            credittimer += 10
+        if pos[0] < 90 and pos[1] > 450 and not credit and credit_timer <= 0:
+            credit_timer += 10
             credit = True
-        elif pos[0] < 100 and pos[1] > 450 and credit and credittimer <= 0:
-            credittimer += 10
+        elif pos[0] < 100 and pos[1] > 450 and credit and credit_timer <= 0:
+            credit_timer += 10
             credit = False
-        elif not credit and credittimer <= 0:
+        elif not credit and credit_timer <= 0:
             pygame.mixer.music.stop()
             menuOpen = False
 
@@ -314,17 +314,17 @@ while running:
         if e.type == pygame.QUIT:
             running = False
         if e.type == USEREVENT + 1:
-            carstage += 1
+            car_stage += 1
         if e.type == USEREVENT + 2:
             if char.speed < 6.9:
                 char.speed += 0.1
-            if enemyspeed < 10:
-                enemyspeed += 0.1
+            if enemy_speed < 10:
+                enemy_speed += 0.1
         if e.type == USEREVENT + 4 and ticks <= 40 and not gameOver and not menuOpen:
             obstacle()
-        if e.type == USEREVENT + 5 and ticks >= 41 and ticks <= 80 and not gameOver and not menuOpen:
+        if e.type == USEREVENT + 5 and 41 <= ticks <= 80 and not gameOver and not menuOpen:
             obstacle()
-        if e.type == USEREVENT + 6 and ticks >= 81 and ticks <= 99 and not gameOver and not menuOpen:
+        if e.type == USEREVENT + 6 and 81 <= ticks <= 99 and not gameOver and not menuOpen:
             obstacle()
         if e.type == USEREVENT + 7 and ticks >= 100 and not gameOver and not menuOpen:
             obstacle()
@@ -335,8 +335,8 @@ while running:
         bgy1 += char.speed
         bgy2 += char.speed
 
-        if carstage >= 2:
-            carstage = 0
+        if car_stage >= 2:
+            car_stage = 0
 
         score += 1
 
